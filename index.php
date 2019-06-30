1f67dd51b18771e6e5935a1761f403fc054637aa
Amanditas <allonsmandy@gmail.com>
2019-06-30 01:06:45 -0300

    barra de admin - wp_head e wp_footer

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
     <title>Tema básico</title>
+    <?php wp_head(); ?>
 </head>
 <body>
 
     <h1><?php bloginfo('name'); ?></h1>     <!-- mostra o nome do blog dinamicamente -->
     <strong><?php bloginfo('description'); ?></strong>     <!-- mostra a descrição do blog -->
+
+    <?php wp_footer(); ?>
 </body>
 </html>
