e81f0901e69188579bca4f34aaae56aaffe6bd5a
Amanditas <allonsmandy@gmail.com>
2019-06-30 02:57:40 -0300

    categoria e data do post - the_category e the_time

--- index.php
+++ index.php
@@ -12,6 +12,9 @@
             </a>
         </h1>
 
+        <span><?php the_category(', '); ?> </span> <!-- exibe as categorias do post -->
+        <span><?php the_time('d/m/Y - h:i:s'); ?></span> <!-- exibe a data e hora do post -->
+
         <p class="post"><?php the_excerpt(); ?></p> <!-- faz um resumo do post com 55 palavras -->
         
         <?php endwhile; ?>
