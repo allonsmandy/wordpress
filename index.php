ebb9f1b153e266c6ccce8992dbc74a3f7dc57039
Amanditas <allonsmandy@gmail.com>
2019-06-30 01:15:15 -0300

    tags de inclusao - get header e get footer

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Tema básico</title>
-    <?php wp_head(); ?>
-</head>
-<body>
+<?php get_header(); ?> <!-- pega o header.php -->
 
     <h1><?php bloginfo('name'); ?></h1>     <!-- mostra o nome do blog dinamicamente -->
     <strong><?php bloginfo('description'); ?></strong>     <!-- mostra a descrição do blog -->
 
-    <?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?> <!-- pega o footer.php -->
