9f42c3d491bdb798393cd8f751c1682833c0a567
Amanditas <allonsmandy@gmail.com>
2019-06-30 05:55:59 -0300

    praticando um exemplo de tags condicionais

--- category.php
+++ category.php
@@ -1,9 +1,17 @@
 <?php get_header(); ?>
 
+    <!-- Se esiver na categoria design, carrega o banner -->
+    <?php if(is_category('design')) : ?>
+        <img src="<?php bloginfo('template_url'); ?>/img/banner-design.png" >
+    <?php else: ?>
+    <!-- se não, carrega o titulo -->
+
     <h2 class="title-category">Hey baby, tu estás na categoria:
         <?php single_cat_title(); ?> <!-- titulo da categoria -->
     </h2>
     <span><?php echo category_description(); ?></span> <!-- descrição da categoria -->
+    
+    <?php endif; ?>
 
        <!-- POSTS -->
        
