61e5349d5290b298ce1d3a851363859e7e5fda6f
Amanditas <allonsmandy@gmail.com>
2019-06-30 01:48:45 -0300

    loop dos posts

--- index.php
+++ index.php
@@ -3,4 +3,13 @@
     <h1><?php bloginfo('name'); ?></h1>     <!-- mostra o nome do blog dinamicamente -->
     <strong><?php bloginfo('description'); ?></strong>     <!-- mostra a descrição do blog -->
 
+    <!-- POSTS -->
+    <?php while(have_posts()) : the_post();  ?> <!-- ver se tem posts ou não -->
+        
+        <h1 class="titulo-post"><?php the_title(); ?></h1> <!-- pega o titulo do post -->
+        <p class="post"><?php the_content("Continue lendo, baby..."); ?></p> <!-- pega o conteudo do post -->
+        
+        <?php endwhile; ?>
+    <!-- FIM DO LOOP DE POSTS -->
+
 <?php get_footer(); ?> <!-- pega o footer.php -->
