68952d1b67dfda7fb6f7125cfb0d98fbbee3c1c1
Amanditas <allonsmandy@gmail.com>
2019-07-05 02:03:15 -0300

    exibe banner dinamico apenas na home do site

--- header.php
+++ header.php
@@ -12,11 +12,11 @@
 
 <header>
 
-    <?php if(has_header_image()): ?>
+        <?php if(has_header_image() && is_home()) : ?>  <!-- exibe se tiver uma imagem no header e se estiver na home do site -->
 
-        <img src="<?php header_image(); ?>"> <!-- pega o endereço da imagem do header que foi adicionado dinamicamente -->
-        <h1><?php bloginfo('description'); ?></h1> <!-- descrição do site -->
-    
-    <?php endif; ?>
+            <img src="<?php header_image(); ?>"> <!-- pega o endereço da imagem do header que foi adicionado dinamicamente -->
+            <h1><?php bloginfo('description'); ?></h1> <!-- descrição do site -->
+        
+        <?php endif; ?>
 
 </header>
