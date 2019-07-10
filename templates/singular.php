57dd40c4b42edc29fd93621b5fe9899e152cb04e
Amanditas <allonsmandy@gmail.com>
2019-07-10 03:40:09 -0300

    criando um embed do youtube com campos customizados

--- templates/singular.php
+++ templates/singular.php
@@ -1,3 +1,14 @@
+<?php if(get_post_meta(get_the_ID(), 'id_video', true)): ?>
+    <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/<?php echo get_post_meta(get_the_ID(), 'id_video', true); ?>"
+        frameborder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen>
+    </iframe>
+<?php endif; ?>
+
 <h2> <?php the_title(); ?> </h2>
 <p> <?php the_content(); ?> </p>
 
