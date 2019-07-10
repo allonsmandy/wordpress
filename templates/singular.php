e943264eb9b92065d04272959ab921c5baecd617
Amanditas <allonsmandy@gmail.com>
2019-07-10 03:20:02 -0300

    campos customizados e suas condicionais especificas para exibiçao

--- templates/singular.php
+++ templates/singular.php
@@ -1,4 +1,11 @@
 <h2> <?php the_title(); ?> </h2>
 <p> <?php the_content(); ?> </p>
 
+<p>Categorias: <?php the_category(', ') ?></p>
+
+<!--  Exibe o custom-field caso o slug cadastrado seja post_level e tenha a categoria desenvolvimento-->
+<?php if(get_post_meta(get_the_ID(), 'post_level', true) && has_category('Desenvolvimento')): ?>
+    Nivel do post: <?php echo get_post_meta(get_the_ID(), 'post_level', true); ?>
+<?php endif; ?>
+
 <a href="<?php bloginfo('url'); ?>">Voltar para a home </a>
