e2fcb4ecbd8521f667154fc6b4e7e575a5b1a4e8
Amanditas <allonsmandy@gmail.com>
2019-07-12 03:39:36 -0300

    trabalhando com o plugin advanced custom field e com as funçoes get_field e the_field

--- templates/singular.php
+++ templates/singular.php
@@ -1,13 +1,16 @@
-<?php if(get_post_meta(get_the_ID(), 'id_video', true)): ?>
+<?php 
+if(function_exists('get_field')): //só exibe as funçoes get field se o plugin adf estiver ativado
+    if(get_field('id_video')): ?>
     <iframe 
         width="560" 
         height="315" 
-        src="https://www.youtube.com/embed/<?php echo get_post_meta(get_the_ID(), 'id_video', true); ?>"
+        src="https://www.youtube.com/embed/<?php the_field('id_video'); ?>"
         frameborder="0" 
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
         allowfullscreen>
     </iframe>
-<?php endif; ?>
+<?php endif; 
+    endif; ?>
 
 <h2> <?php the_title(); ?> </h2>
 <p> <?php the_content(); ?> </p>
@@ -15,8 +18,11 @@
 <p>Categorias: <?php the_category(', ') ?></p>
 
 <!--  Exibe o custom-field caso o slug cadastrado seja post_level e tenha a categoria desenvolvimento-->
-<?php if(get_post_meta(get_the_ID(), 'post_level', true) && has_category('Desenvolvimento')): ?>
-    Nivel do post: <?php echo get_post_meta(get_the_ID(), 'post_level', true); ?>
-<?php endif; ?>
+<?php 
+if(function_exists('get_field')): 
+if(get_field('post_level') && has_category('Desenvolvimento')): ?>
+    Nivel do post: <?php the_field('post_level'); ?>
+<?php endif; 
+    endif; ?>
 
 <a href="<?php bloginfo('url'); ?>">Voltar para a home </a>
