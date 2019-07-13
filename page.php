79406ca7845245d8522637434299e16d571f41a1
Amanditas <allonsmandy@gmail.com>
2019-07-13 11:51:26 -0300

    trabalhando com a funcao setup_postdata

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
 
     <?php while(have_posts()) : the_post(); ?>
 
-        <?php get_template_part('templates/singular'); ?>
+        <?php get_template_part('templates/card', 'page'); ?>
         <?php get_template_part('templates/navigation', 'single') ?> <!-- navegação em uma pagina single -->
 
     <?php endwhile; ?>
--- templates/card-page.php
+++ templates/card-page.php
@@ -4,10 +4,12 @@
 
 <!-- CARREGANDO INFORMAÇÕES DO OBJETO POST -->
 <?php
-    $extras = get_field('extras');
+    $post = get_field('extras');
 
-    echo '<h3>' . $extras->post_title . '</h3>';
-    echo $extras->post_content;
+    setup_postdata($post);
+    echo '<h3>' . get_the_title() . '</h3>';
+    echo get_the_content();
+    wp_reset_postdata();
 ?>
 
 <a href="<?php bloginfo('url'); ?>">Voltar para a home </a>
